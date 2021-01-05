253606e4c46e17bc40ac3534fe8740d6351588d6
black-sliver <>
2021-01-05 17:05:13 +0100

    add support for patch sets

--- everscript2h.py
+++ everscript2h.py
@@ -21,9 +21,21 @@ if __name__ == '__main__':
         for i in range(firstinput,len(argv)):
             n = 1
             name = os.path.splitext(os.path.basename(argv[i]))[0].upper()
+            in_set = False
+            set_name = ''
+            set_items = []
             with open(argv[i]) as fin:
                 in_patch = False
+                line_no = 0
                 for line in fin:
+                  line_no += 1
+                  try:
+                    if line.lstrip().startswith('#set'):
+                        in_set = True
+                        set_name = line.lstrip()[4:].lstrip().split('//')[0].rstrip()
+                        if set_name == '': set_name = name
+                        set_items = []
+                        continue
                     if line.lstrip().startswith('#'):
                         fout.write(line.encode('ascii'))
                         continue
@@ -47,6 +59,7 @@ if __name__ == '__main__':
                             fout.write(b'DEF(%b, 0x%06x - 0x800000,\n' % (patchname.encode('ascii'), addr))
                         else: # assume absolute address
                             fout.write(b'DEF(%b, 0x%06x,\n' % (patchname.encode('ascii'), addr))
+                        if in_set: set_items.append(b'%b' % (patchname.encode('ascii'),))
                         in_patch = True
                         continue
                     if '//' in line:
@@ -68,6 +81,19 @@ if __name__ == '__main__':
                         if comment!='': comment=' //'+comment
                         b = data.split(' ')
                         fout.write(b'%s"\\x%s"%s\n' % (spaces[in_patch], '\\x'.join(b).encode('ascii'), comment.encode('ascii')))
+                  except Exception as e:
+                    print('In %s:%d' % (os.path.basename(argv[i]), line_no))
+                    raise(e)
+                    
                 if in_patch:
                     fout.write(b');\n');
+                
+                if in_set and set_name != '':
+                    # add define to apply all previous since #set directive
+                    fout.write(b'#define APPLY_%b() do {' % (set_name.encode('ascii'),))
+                    i = 0
+                    for i in range(len(set_items)):
+                        fout.write( b'\\\n    ' if i%4==0 else b' ' )
+                        fout.write(b'APPLY(%b);' % (set_items[i],))
+                    fout.write(b'} while(false)\n')
 
