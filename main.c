814dfb16cccb41da9326a6577cd91eee3a3158fe
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-02-03 02:00:34 +0100

    bump version to v043

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 // see google doc for documentation on old (numbered) patches
 // the idea is to "manually" patch the game to a state where we simply swap
 // out some numbers to make it random (without rewriting/relocating everything)
-#define VERSION "v042"
+#define VERSION "v043"
 
 #include <stdio.h>
 #include <stdlib.h>
