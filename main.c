9e80d4b7e7ebaa1e55387a169cfbb681eafda595
black-sliver <59490463+black-sliver@users.noreply.github.com>
2024-03-28 20:59:29 +0100

    bump version to v046
    and update changelog

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 // see google doc for documentation on old (numbered) patches
 // the idea is to "manually" patch the game to a state where we simply swap
 // out some numbers to make it random (without rewriting/relocating everything)
-#define VERSION "v046"
+#define VERSION "v047"
 
 #include <stdio.h>
 #include <stdlib.h>
